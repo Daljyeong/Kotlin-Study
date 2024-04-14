d047a63af1d757f1cffa5b6babd9c9ba8dae7e5a
Daljyeong <jiyoungm1022@gmail.com>
2024-04-14 18:45:45 +0900

    feat: TTS 기능 추가한 VocList2.kt 파일 생성 (모프 06주차 실습)

--- Week06C_wordbook/app/src/main/java/com/example/week06c_wordbook/screen/MainScreen.kt
+++ Week06C_wordbook/app/src/main/java/com/example/week06c_wordbook/screen/MainScreen.kt
@@ -4,13 +4,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.week06c_wordbook.component.MainTitle
-import com.example.week06c_wordbook.component.vocList
+import com.example.week06c_wordbook.component.vocList2
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     // 단어장 만들기
     Column {
-        MainTitle() // 단어장 title
-        vocList() // 단어 list
+        // 단어장 title
+        MainTitle()
+
+        // 단어 list
+//        vocList()
+        vocList2()
     }
 }
